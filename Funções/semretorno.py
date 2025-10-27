7431ffe0acc7ac0928e6178194c61c2ac2ceb070
Emylli Soares <100449171+emyllisoares@users.noreply.github.com>
2025-10-27 14:54:20 +0000

    Função sem retorno

---
+++ Funções/semretorno.py
@@ -0,0 +1,11 @@
+def mostra_rodape():
+    print("------------------------")
+    print("| Emylli Soares Santos |")
+    print("------------------------")
+
+print("Execução do programa")
+mostra_rodape()
+print("Execução de mais comandos")
+mostra_rodape()
+
+
